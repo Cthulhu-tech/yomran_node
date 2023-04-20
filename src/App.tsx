cd996655bd25cd47dc2ca9cd59d2939653df330f
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-04-20 15:16:43 +0400

    header create

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { Header } from './components/layouts/header/header'
 import { ElectronWindow } from './interface/electron'
+
+import './style/global.scss'
 
 declare const window: ElectronWindow
 
 export const App = () => {
   return <>
+      <Header/>
         <h1>Hello React</h1>
       <button onClick={() => {
         window.api.notification({message: 'My custom Notification', chat: 'chat'});
---
+++ src/components/layouts/header/header.tsx
@@ -0,0 +1,35 @@
+import { ElectronWindow } from "../../../interface/electron"
+import { useState } from "react"
+
+import './header.scss'
+
+declare const window: ElectronWindow
+
+export const Header = () => {
+
+    const close = () => window.api.closeAplication()
+    const min = () => window.api.minAplication()
+    const max = () => {
+        setFullscreen((fullscreen) => !fullscreen)
+        window.api.maxAplication()
+    }
+
+    const [fullscreen, setFullscreen] = useState(false)
+    
+    return <header className="header">
+        <div className="header__drag">
+            
+        </div>
+        <div className="header__icon-container">
+                <div className="header__icon minimize" onClick={min}>
+                    
+                </div>
+                <div className="header__icon maximize" onClick={max}>
+                    
+                </div>
+                <div className="header__icon close" onClick={close}>
+                
+                </div>
+            </div>
+    </header>
+}
