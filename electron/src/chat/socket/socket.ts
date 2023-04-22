d4f8414ebab900c236f9af49c54063c931ebc688
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-04-22 16:59:54 +0400

    socket connection create

---
+++ electron/src/chat/socket/socket.ts
@@ -0,0 +1,22 @@
+import { Server, Socket } from 'socket.io';
+import { createServer } from 'http';
+
+export class SocketServer {
+    constructor() {}
+    createSocketServer = async (port: number) => {
+        const httpServer = await createServer()
+
+        const io = new Server(httpServer, {
+            cors: {
+                origin: '*'
+            }
+        })
+
+        io.on("connection", (socket: Socket) => {
+            console.log('connection', socket)
+            socket.emit('hello', 'world')
+        })
+
+        httpServer.listen(port)
+    }
+}
