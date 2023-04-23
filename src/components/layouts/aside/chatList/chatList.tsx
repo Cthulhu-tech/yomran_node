e34c1f73af4b0ac501971842b3fdf337a7169150
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-04-23 20:29:39 +0400

    icon add

--- src/components/layouts/aside/chatList/chatList.tsx
+++ src/components/layouts/aside/chatList/chatList.tsx
@@ -1,7 +1,9 @@
 import { Chat } from '../../../../../electron/src/sql/entity/chat'
 import { ElectronWindow } from '../../../../interface/electron'
+import { useTranslation } from "react-i18next"
 import { useEffect, useState } from 'react'
 
+import { Trash } from '../../../icon/trash'
 import { NavLink } from "react-router-dom"
 
 declare const window: ElectronWindow
@@ -10,24 +12,36 @@ export const ChatList = () => {
 
     const [chat, setChat] = useState<Chat[]>()
 
+    const { t } = useTranslation()
+
     const getChat = async () => setChat(await window.api.get_all_chat_info())
+
+    const deleteChat = async (id: number) => 
+        await window.api.delete_chat_by_id({id: Number(id)})
+            .then(() => {
+                setChat((chatData) => chatData?.filter((_chat) => _chat.id != id))
+            })
 
     useEffect(() => { getChat() }, [])
 
     return  <section className='flex flex-col w-full'>
         <ul className="flex flex-col py-4 space-y-1">
             <li className="px-5">
-                <div className="flex flex-row items-center h-8">
-                    <p className="text-sm font-light tracking-wide text-gray-500">Chat list</p>
+                <div className="flex flex-row items-center h-8 w-full">
+                    <p className="text-sm font-light tracking-wide w-full text-gray-500 text-center pb-2 select-none">{ t('chat_list_name') }</p>
                 </div>
             </li>
             <li>
                 {chat?.map((_chat) =>
-                    <NavLink to={"/chat/" + _chat.id} key={_chat.id} className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
-                        <span className="inline-flex justify-center items-center ml-4">
-                        </span>
+                    <div key={_chat.id} className="flex items-center relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                    <NavLink to={"/chat/" + _chat.id} className='w-full' >
+                        <span className="inline-flex justify-center items-center ml-4"></span>
                         <span className="ml-2 text-sm tracking-wide truncate">{_chat?.name}</span>
                     </NavLink>
+                        <div onClick={() => deleteChat(_chat.id)}>
+                            <Trash  width={18} height={18}/>
+                        </div>
+                    </div>                    
                 )}
             </li>
         </ul>
--- src/components/layouts/aside/create/create.tsx
+++ src/components/layouts/aside/create/create.tsx
@@ -1,5 +1,6 @@
 import { JWTdecode } from '../../../../../electron/src/chat/type'
 import { ElectronWindow } from '../../../../interface/electron'
+import { useTranslation } from "react-i18next"
 import { io } from 'socket.io-client'
 
 const jwt_decode = require('jwt-decode')
@@ -8,6 +9,8 @@ declare const window: ElectronWindow
 
 export const Create = () => {
 
+    const { t } = useTranslation()
+
     const create = async () => {
         const data = await window.api.create_chat({chat_name: 'chat', password: '123'})
         const decodedJWT: JWTdecode = jwt_decode(data)
@@ -15,9 +18,9 @@ export const Create = () => {
         const socket = io(decodedJWT.ipV4 + ':' + decodedJWT.port)
     }
 
-    return  <button onClick={create} className="ml-2 mr-2 w-48 mb-4 mt-4 relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-        <span className="w-48 relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-            Create
+    return  <button onClick={create} className="w-72 mb-4 mt-4 relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+        <span className="w-full relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+            { t('create') }
         </span>
     </button>
 }
