a35de243b1267be5104e5d51b514858fc0b801af
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-04-23 16:31:08 +0400

    update electron

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
-import './style/global.scss';
-
-import jwt_decode from 'jwt-decode';
-import { io } from 'socket.io-client';
-
-import { JWTdecode } from '../electron/src/chat/type';
 import { Header } from './components/layouts/header/header';
-import { ElectronWindow } from './interface/electron';
-
-declare const window: ElectronWindow
+import { Aside } from './components/layouts/aside/aside';
+import './style/global.scss';
   
 export const App = () => {
 
-  const create = async () => {
-    const data = await window.api.create_chat({chat_name: 'chat', password: '123'})
-    const decodedJWT: JWTdecode = jwt_decode(data)
-
-    const socket = io(decodedJWT.ipV4 + ':' + decodedJWT.port)
-  }
 
   return <>
       <Header/>
-        <h1>Hello React</h1>
-      <button onClick={create}>Click me</button>
+      <Aside/>
   </>
 }
 
--- src/interface/electron.ts
+++ src/interface/electron.ts
@@ -1,4 +1,5 @@
 import { crateChatType, notificationType } from '../../electron/src/interface/interface'
+import { Chat } from '../../electron/src/sql/entity/chat'
 
 export interface ElectronWindow extends Window {
     api: {
@@ -7,5 +8,6 @@ export interface ElectronWindow extends Window {
         maxAplication: ()  => void
         notification: ({ message, chat }: notificationType) => void
         create_chat: ({ chat_name, password }: crateChatType) => Promise<string>
+        get_all_chat_info: () => Promise<Chat[]>
     }
 }
