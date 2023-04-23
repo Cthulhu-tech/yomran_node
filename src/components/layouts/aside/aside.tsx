873e8d6a8e1352eec193ff02910a3f20448a673c
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-04-23 18:22:10 +0400

    chat list and create

--- src/components/layouts/aside/aside.tsx
+++ src/components/layouts/aside/aside.tsx
@@ -1,11 +1,15 @@
 import { ChatList } from './chatList/chatList'
 import { Create } from './create/create'
 import './aside.scss'
+import React from 'react'
+
+const ChatListMemo = React.memo(ChatList)
+const CreateMemo = React.memo(Create)
 
 export const Aside = () => {
 
-    return <aside>
-        <ChatList/>
-        <Create/>
+    return <aside className='h-full shadow-sm border-r flex flex-col w-64 bg-white justify-between items-center'>
+        <ChatListMemo/>
+        <CreateMemo/>
     </aside>
 }
--- src/components/layouts/aside/chatList/chatList.tsx
+++ src/components/layouts/aside/chatList/chatList.tsx
@@ -2,6 +2,8 @@ import { Chat } from '../../../../../electron/src/sql/entity/chat'
 import { ElectronWindow } from '../../../../interface/electron'
 import { useEffect, useState } from 'react'
 
+import { NavLink } from "react-router-dom"
+
 declare const window: ElectronWindow
 
 export const ChatList = () => {
@@ -12,11 +14,22 @@ export const ChatList = () => {
 
     useEffect(() => { getChat() }, [])
 
-    return <section>
-        {chat?.map((_chat) =>
-            <div key={_chat.id}>
-                <p>{_chat?.name}</p>
-            </div>
-        )}
+    return  <section className='flex flex-col w-64'>
+        <ul className="flex flex-col py-4 space-y-1">
+            <li className="px-5">
+                <div className="flex flex-row items-center h-8">
+                    <p className="text-sm font-light tracking-wide text-gray-500">Chat list</p>
+                </div>
+            </li>
+            <li className='border-r'>
+                {chat?.map((_chat) =>
+                    <NavLink to={"/chat/" + _chat.id} key={_chat.id} className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
+                        <span className="inline-flex justify-center items-center ml-4">
+                        </span>
+                        <span className="ml-2 text-sm tracking-wide truncate">{_chat?.name}</span>
+                    </NavLink>
+                )}
+            </li>
+        </ul>
     </section>
 }
