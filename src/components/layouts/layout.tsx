ff4bc5e69c1ae1c4ee395877bdbdb8520e947556
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-04-25 05:24:20 +0400

    update layout

--- src/components/layouts/layout.tsx
+++ src/components/layouts/layout.tsx
@@ -1,18 +1,21 @@
 import { Header } from "./header/header"
 import { Aside } from "./aside/aside"
+import { Menu } from "./menu/menu"
 
-import { Outlet } from 'react-router-dom';
-import React from "react";
+import { Outlet } from 'react-router-dom'
+import React from "react"
 
-const AsideMemo = React.memo(Aside)
 const HeaderMemo = React.memo(Header)
+const AsideMemo = React.memo(Aside)
+const MenuMemo = React.memo(Menu)
 
 export const Layout = () => {
 
     return <>
         <HeaderMemo/>
         <AsideMemo/>
-        <main className="main overflow-hidden">
+        <MenuMemo/>
+        <main className="main bg-slate-100 rounded-md overflow-hidden shadow-sm mt-2 mr-2 mb-2 ml-2">
             <Outlet />
         </main>
     </>
