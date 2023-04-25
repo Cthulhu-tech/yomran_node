d6c9c249c3a3d08fbae81caca6e684fb28aadb72
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-04-25 05:23:16 +0400

    header update

--- src/components/layouts/header/header.tsx
+++ src/components/layouts/header/header.tsx
@@ -1,5 +1,4 @@
 import { ElectronWindow } from "../../../interface/electron"
-import { useState } from "react"
 
 import './header.scss'
 
@@ -9,25 +8,21 @@ export const Header = () => {
 
     const close = () => window.api.closeAplication()
     const min = () => window.api.minAplication()
-    const max = () => {
-        setFullscreen((fullscreen) => !fullscreen)
-        window.api.maxAplication()
-    }
+    const max = () => window.api.maxAplication()
 
-    const [fullscreen, setFullscreen] = useState(false)
     
-    return <header className="header shadow-sm">
+    return <header className="header bg-indigo-500">
         <div className="header__drag">
             
         </div>
         <div className="header__icon-container">
-                <div className="header__icon minimize" onClick={min}>
+                <div className="header__icon minimize hover:bg-indigo-100 before:hover:bg-indigo-500 before:bg-indigo-100" onClick={min}>
                     
                 </div>
-                <div className={fullscreen ? "header__icon maximize-max" : "header__icon maximize-min"} onClick={max}>
+                <div className="header__icon maximize-min hover:bg-indigo-100 before:bg-indigo-100 after:hover:bg-indigo-500 before:hover:bg-indigo-500" onClick={max}>
                     
                 </div>
-                <div className="header__icon close" onClick={close}>
+                <div className="header__icon close hover:bg-indigo-100 before:bg-indigo-100 after:bg-indigo-100 after:hover:bg-indigo-500 before:hover:bg-indigo-500" onClick={close}>
                 
                 </div>
             </div>
