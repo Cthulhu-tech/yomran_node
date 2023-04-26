410f7d8418d3a71781d99f9b3dc4617e7cf6928b
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-04-26 06:33:17 +0400

    update layouts

--- src/components/layouts/layout.tsx
+++ src/components/layouts/layout.tsx
@@ -2,7 +2,7 @@ import { Header } from "./header/header"
 import { Aside } from "./aside/aside"
 import { Menu } from "./menu/menu"
 
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import React from "react"
 
 const HeaderMemo = React.memo(Header)
@@ -11,12 +11,14 @@ const MenuMemo = React.memo(Menu)
 
 export const Layout = () => {
 
-    return <>
+    const location = useLocation()
+
+    return <div className={location.pathname !== '/socket' ? 'flex flex-col h-screen start' : 'flex flex-col h-screen start-socket'}>
         <HeaderMemo/>
-        <AsideMemo/>
+        {location.pathname !== '/socket' && <AsideMemo/>}
         <MenuMemo/>
         <main className="main bg-slate-100 rounded-md overflow-hidden shadow-sm mt-2 mr-2 mb-2 ml-2">
             <Outlet />
         </main>
-    </>
+    </div>
 }
