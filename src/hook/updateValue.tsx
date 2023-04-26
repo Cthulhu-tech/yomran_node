5f857dc3ab2fd3763d8c2a5781960f7e316e0ea0
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-04-26 23:34:29 +0400

    new hook add

--- src/hook/updateValue.tsx
+++ src/hook/updateValue.tsx
@@ -3,7 +3,7 @@ import { ChangeEvent, useEffect, useState } from "react"
 export const UpdateValueHook = <T,>(defaultValue: T, validation: (data: T) => T) => {
 
     const [state, setState] = useState<T>(defaultValue)
-    const [error, setError] = useState<T>()
+    const [error, setError] = useState<T>({} as T)
 
     const callback = (event: ChangeEvent<HTMLInputElement>) => setState({...state, [event.target.name]: event.target.value})
 
@@ -11,5 +11,5 @@ export const UpdateValueHook = <T,>(defaultValue: T, validation: (data: T) => T)
         setError(validation(state))
     }, [state])
 
-    return { state, callback, error, setState }
+    return { state, callback, error, setState, setError }
 }
