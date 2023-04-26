b6bbb6ffbaf000ec2190caa059cfa057b1e341aa
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-04-26 23:32:06 +0400

    update

--- src/components/layouts/aside/open/open.tsx
+++ src/components/layouts/aside/open/open.tsx
@@ -1,7 +1,6 @@
 import { setButtonPopup, setOpenPopup } from '../../../../redux/store/open'
 import { setDecodeSocketIO } from '../../../../redux/store/socket'
 import { JWTdecode } from '../../../../../electron/src/chat/type'
-import { setUserLoginData } from '../../../../redux/store/user'
 import { IStore, PopupDefault } from '../../../../redux/type'
 import { useDispatch, useSelector } from 'react-redux'
 import { InputComponent } from '../../../input/input'
@@ -17,19 +16,18 @@ const PopupMemo = React.memo(Popup)
 export const Open = () => {
 
     const [token, setToken] = useState('')
-    const [login, setLogin] = useState('')
 
     const { t } = useTranslation()
     const dispatch = useDispatch()
     const navigate = useNavigate()
 
-    const open = () => {
+    const open = useCallback(() => {
         dispatch(setOpenPopup(true))
         dispatch(setButtonPopup({
             first_btn: 'connect',
             last_btn: 'cancel',
         }))
-    }
+    }, [])
 
     const popupStore = useSelector<IStore, PopupDefault<boolean>>((store) => store.PopupStore)
 
@@ -37,20 +35,17 @@ export const Open = () => {
         const decodedJWT: JWTdecode = jwt_decode(token)
 
         dispatch(setDecodeSocketIO(decodedJWT))
-        dispatch(setUserLoginData(login))
 
         navigate('socket')
-    }, [token, login])
+    }, [token])
 
     const tokenUpdate = (event: React.ChangeEvent<HTMLInputElement>) => setToken(event.target.value)
-    const loginUpdate = (event: React.ChangeEvent<HTMLInputElement>) => setLogin(event.target.value)
 
     return  <>
     {(popupStore.open && popupStore.first_btn === 'connect') && 
     <PopupMemo callback={connect}>
         <div className='w-full'>
             <InputComponent placeholder='token' name="token" type="text" value={token} change={tokenUpdate}/>
-            <InputComponent placeholder='login' name="login" type="text" value={login} change={loginUpdate}/>
         </div>
     </PopupMemo>}
     <button onClick={open} className="w-56 mb-4 mt-4 inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 transition rounded-lg group bg-indigo-200 hover:bg-indigo-500">
