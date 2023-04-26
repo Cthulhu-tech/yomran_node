ef79285595fd521ee9852330ec62e387a51d1102
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-04-26 06:34:28 +0400

    root update

--- src/redux/root.tsx
+++ src/redux/root.tsx
@@ -1,8 +1,14 @@
 import { combineReducers, createStore } from "redux"
+import { MessageStore } from "./store/message"
+import { SocketStore } from "./store/socket"
 import { PopupStore } from "./store/open"
+import { UserStore } from "./store/user"
 
 export const rootReducer = combineReducers({
     PopupStore,
+    SocketStore,
+    MessageStore,
+    UserStore,
 })
 
 export const store = createStore(rootReducer)
--- src/redux/type.tsx
+++ src/redux/type.tsx
@@ -1,3 +1,5 @@
+import { Message } from "../../electron/src/sql/entity/message"
+import { JWTdecode } from "../../electron/src/chat/type"
 
 export interface IAction<T, P> {
     readonly type: T
@@ -6,6 +8,9 @@ export interface IAction<T, P> {
 
 export interface IStore {
     PopupStore: PopupDefault
+    SocketStore: SocketDefault
+    MessageStore: MessageDefault
+    UserStore: UserDefault
 }
 
 export interface PopupDefault<T = any> extends PopupButtonType {
@@ -16,4 +21,23 @@ export interface PopupDefault<T = any> extends PopupButtonType {
 export type PopupButtonType = {
     first_btn: string
     last_btn: string 
+}
+
+export type SocketDefault = {
+    decode: JWTdecode | null
+}
+
+export type MessageDefault = {
+    message: Message[]
+}
+
+export type UserDefault = {
+    user: UserType | null
+}
+
+export type UserType = {
+    creater: boolean
+    id: number
+    login: string
+    socket: string
 }
