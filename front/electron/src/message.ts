c439199a689cbf139f5271af546dcac6dba8ebf3
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-04-30 17:09:06 +0400

    message add

---
+++ front/electron/src/message.ts
@@ -0,0 +1,21 @@
+import { notificationType } from './interface/interface'
+
+const path = require('path')
+const { app } = require('electron')
+const notifier = require('node-notifier')
+
+export class Message {
+    private ASSETS_PATH: string
+    constructor() {
+        this.ASSETS_PATH = app.isPackaged ? path.join(process.resourcesPath, 'assets') : path.join(__dirname, `../../../public${path.sep}assets`)
+    }
+    public newMessageAlarm = async ({ message, chat }: notificationType) => {
+
+        notifier.notify({
+            title: `Notification in chat ${chat}`,
+            message,
+            icon: path.join(this.ASSETS_PATH, 'message.png'),
+            sound: true,
+          });
+    }
+}
