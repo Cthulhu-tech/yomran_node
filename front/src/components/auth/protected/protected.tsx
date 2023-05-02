f902f6542d48660bb29be6dfd42b9b5c8be6aedf
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-05-03 02:24:08 +0400

    update

---
+++ front/src/components/auth/protected/protected.tsx
@@ -0,0 +1,16 @@
+import { Navigate, useLocation } from "react-router-dom"
+import { TokenType } from "../../../redux/type"
+import { IStore } from "../../../redux/type"
+import {useSelector} from "react-redux"
+import React from 'react'
+
+export const ProtectedRoute = (props: {children: React.ReactNode}) => {
+
+    const location = useLocation()
+    const user = useSelector<IStore, TokenType>((state) => state.Token)
+    console.log(user)
+    if(!user.access && !location.pathname.includes('auth')) 
+        return <Navigate to="/auth" state={{ from: location }} replace />
+
+    return <>{props.children}</>
+}
