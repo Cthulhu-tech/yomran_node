d367e4dcf47779de8f64c82949545d052a381e1d
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-05-02 14:19:10 +0400

    update view

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,16 +1,53 @@
-import { createHashRouter, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, createHashRouter, Navigate, Outlet, RouterProvider } from "react-router-dom"
+import { Registration } from "./view/auth/registration/registration"
 import { Layout } from './components/layouts/layout'
-
+import { Login } from "./view/auth/login/login"
+import { Auth } from "./view/auth/auth"
 import './style/global.scss'
-
 import './plugin/i18n'
+
+import { RefreshLoader } from "./components/layouts/loader"
 
 export const App = () => {
 
-  const router = createHashRouter([
+  const router = createBrowserRouter([
     {
       path: '/',
-      element: <Layout/>,
+      loader: RefreshLoader,
+      id: 'refresh',
+      errorElement: <Navigate to='auth'/>,
+      children: [
+        {
+          index: true,
+          element: <Layout/>
+        },
+      ],
+    },
+    {
+      path: 'auth',
+      children: [
+        {
+          element: <Layout/>,
+          children: [
+            {
+              index: true,
+              element: <Auth/>
+            },
+            {
+              path: 'login',
+              element: <Login/>
+            },
+            {
+              path: 'registration',
+              element: <Registration/>
+            }
+          ]
+        },
+      ]
+    },
+    {
+      path: '*',
+      element: <>Not found</>
     }
   ])
 
