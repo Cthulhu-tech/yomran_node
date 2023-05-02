1df34935a5eacf2300ef543f0bb12cfc2d768bc0
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-05-03 02:25:12 +0400

    loading update

---
+++ front/src/components/loading/loading.tsx
@@ -0,0 +1,13 @@
+import './loading.scss'
+
+export const Loading = () => {
+
+    return  <div className="loading-screen">
+        <div className="loading">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+}
