8ead2646cb9f46bf3729860ae281fcc59a0699a6
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-05-03 02:25:30 +0400

    update

--- front/src/components/auth/notProtected/notProtected.tsx
+++ front/src/components/auth/notProtected/notProtected.tsx
@@ -4,13 +4,13 @@ import { IStore } from "../../../redux/type"
 import {useSelector} from "react-redux"
 import React from 'react'
 
-export const ProtectedRoute = (props: {children: React.ReactNode}) => {
+export const NotProtectedRoute = (props: {children: React.ReactNode}) => {
 
     const location = useLocation()
     const user = useSelector<IStore, TokenType>((state) => state.Token)
 
-    if(!user.access && !location.pathname.includes('auth')) 
-        return <Navigate to="/auth" state={{ from: location }} replace />
+    if(user.access && location.pathname.includes('auth')) 
+        return <Navigate to="/" replace={true} />
 
     return <>{props.children}</>
 }
