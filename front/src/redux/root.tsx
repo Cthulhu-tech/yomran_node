87ea52a7ab9e626f057d81ab7709ee9f30507cbc
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-05-02 14:16:17 +0400

    update store type

--- front/src/redux/root.tsx
+++ front/src/redux/root.tsx
@@ -1,7 +1,8 @@
 import { combineReducers, createStore } from "redux"
+import { Token } from './token/token'
 
 export const rootReducer = combineReducers({
-
+    Token
 })
 
 export const store = createStore(rootReducer)
--- front/src/redux/type.tsx
+++ front/src/redux/type.tsx
@@ -1,10 +1,16 @@
-
 export interface IAction<T, P> {
     readonly type: T
     readonly payload: P
 }
 
 export interface IStore {
-
+    Token: TokenType
 }
 
+export interface TokenRefresh {
+    access: string
+}
+
+export interface TokenType extends TokenRefresh {
+    loading: boolean
+}
