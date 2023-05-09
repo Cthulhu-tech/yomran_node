e0932b6fe6ac11bfab62121d8f815856264650e9
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-05-10 00:57:35 +0400

    chat add

---
+++ front/src/components/chat/chat.tsx
@@ -0,0 +1,12 @@
+export const Chat = () => {
+
+    return <aside className="container_chat h-full w-full">
+        <div className="chat_name bg-white shadow-inner"></div>
+        <div className="chat_text bg-gray-200 shadow-inner">
+
+        </div>
+        <div className="controll_chat rounded-br-[20px] shadow-inner bg-white">
+
+        </div>
+    </aside>
+}
