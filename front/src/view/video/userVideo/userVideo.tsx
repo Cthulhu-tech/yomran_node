467af1508449d21c5b3fa02eff27aa8302496af4
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-05-10 17:06:53 +0300

    update style

--- front/src/view/video/userVideo/userVideo.tsx
+++ front/src/view/video/userVideo/userVideo.tsx
@@ -2,7 +2,7 @@ import { UserVideoType } from "./type"
 
 import { useEffect } from "react"
 
-export const UserVideo = ({ userJoin, videoUser }: UserVideoType) => {
+export const UserVideo = ({ userJoin, videoUser, hidden }: UserVideoType) => {
 
     useEffect(() => {
         if(videoUser.current)
@@ -10,9 +10,9 @@ export const UserVideo = ({ userJoin, videoUser }: UserVideoType) => {
     }, [videoUser])
 
     return <div className="embed-responsive embed-responsive-16by9 relative w-full overflow-hidden max-w-screen-xl h-full">
-        <div className="embed-responsive-item rounded-xl bg-slate-800 absolute h-full w-full z-10"></div>
+        <div className={hidden ? "embed-responsive-item rounded-xl bg-slate-800 absolute h-full w-full z-50" : "embed-responsive-item rounded-xl bg-slate-800 absolute h-full w-full z-0" }></div>
         <video
-            className="embed-responsive-item rounded-xl bg-slate-800 absolute h-full w-full z-0"
+            className="embed-responsive-item rounded-xl bg-slate-800 absolute h-full w-full z-20"
             ref={videoUser}
         ></video>
     </div>
