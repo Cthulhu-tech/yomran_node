1ce66e6654b6d9adb2bab53adeb0eb7422b49b83
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-05-11 19:04:49 +0400

    update credentials

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors({
-    origin: true,
+    origin: ['http://localhost:3000/'],
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
     credentials: true,
   })
