dd3a9ba13f66a79afd2d84d4d6b041c371442719
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-05-13 23:40:18 +0400

    new store - catlist

---
+++ front/src/redux/chatList/chatList.tsx
@@ -0,0 +1,18 @@
+import { IAction, ChatsType } from "../type"
+
+const defaultState: ChatsType[] = []
+
+export const ChatList = (state = defaultState, action:IAction<string, ChatsType[]>) => {
+    console.log(action.payload)
+    switch (action.type){
+        case 'set_chat_list':
+            return state = [...action.payload]
+        default:
+            return state
+    }
+}
+
+export const updateChatList = (payload: ChatsType[]) => ({
+    type: "set_chat_list",
+    payload,
+})
