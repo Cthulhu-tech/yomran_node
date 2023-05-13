346bc913e1e56e7db5b1803f9547464335aaa8aa
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-05-13 13:19:41 +0400

    update menu

--- front/src/components/layouts/menu/menu.tsx
+++ front/src/components/layouts/menu/menu.tsx
@@ -2,13 +2,14 @@ import { ElectronWindow } from "../../../interface/electron"
 import { updateToken } from "../../../redux/token/token"
 import { IStore, TokenType } from "../../../redux/type"
 import { NavLink, useNavigate } from "react-router-dom"
+import { logoutType, setting } from "./type"
 import { useFetch } from "../../../hook/hook"
 import { useSelector } from "react-redux"
 import { Dropdown } from 'flowbite-react'
 import { useDispatch } from "react-redux"
-import { logoutType } from "./type"
-import { useEffect } from "react"
-import { t } from "i18next"
+
+import { MouseEvent, useEffect, useState } from "react"
+import { useTranslation } from "react-i18next"
 
 declare const window: ElectronWindow
 
@@ -16,6 +17,11 @@ export const Menu = () => {
 
     const dispatch = useDispatch()
     const navigate = useNavigate()
+    const [setting, setSetting] = useState<setting>({
+        user: null,
+        language: 'english'
+    })
+    const { t, i18n } = useTranslation()
     const token = useSelector<IStore, TokenType>((store) => store.Token)
     const { fetchData, returnData, error }= useFetch<undefined, logoutType>('auth/logout', 'POST')
 
@@ -28,12 +34,42 @@ export const Menu = () => {
         }
     }, [returnData])
 
-    const getUserSetting = async (login: string) => await window.api.getUserSetting({ login })
+    const getUserSetting = async (login: string) => {
+        const newSetting = await window.api.getUserSetting({ login })
+        setSetting({
+            user: newSetting,
+            language: newSetting?.settings[0]?.lang ? newSetting?.settings[0]?.lang : setting.language
+        })
+        
+        createUserSetting(login)
+    }
+    const createUserSetting = async (login: string) =>{
+        const newSetting = await window.api.createUserSetting({ login })
+        setSetting({
+            user: newSetting,
+            language: newSetting?.settings[0]?.lang ? newSetting?.settings[0]?.lang : setting.language
+        })
+    }
+    const changeLanguage = async (event: MouseEvent<HTMLSpanElement, globalThis.MouseEvent>) => {
+        let language = event.currentTarget.innerText.toLocaleLowerCase()
+        if(token.user && setting?.user?.settings[0]?.id){
+            const newSetting = await window.api.changeLanguage({ id: setting.user.settings[0].id, language, login: token.user })
+            setSetting({
+                user: newSetting,
+                language
+            })
+        }else {
+            setSetting({
+                user: null,
+                language
+            })
+        }
+        i18n.changeLanguage(language)
+    }
 
     useEffect(() => {
-        if(token.access && token.user) {
-            console.log(getUserSetting(token.user))
-        }
+        if(token.access && token.user)
+            getUserSetting(token.user)
     }, [token])
 
     return <section className="menu shadow-sm mt-2 mr-2 mb-2 ml-2 p-2 rounded-md bg-indigo-100">
@@ -57,14 +93,14 @@ export const Menu = () => {
             </div>}
             <div className="m-2">
                 <Dropdown
-                    label={token.user}
+                    label={setting.language.toLocaleUpperCase()}
                     inline={true}
                 >
                     <Dropdown.Item>
-                        <span>English</span>
+                        <span onClick={changeLanguage}>English</span>
                     </Dropdown.Item>
                     <Dropdown.Item>
-                        <span>Русский</span>
+                        <span onClick={changeLanguage}>Русский</span>
                     </Dropdown.Item>
                 </Dropdown>
             </div>
--- front/src/components/layouts/menu/type.tsx
+++ front/src/components/layouts/menu/type.tsx
@@ -1,3 +1,10 @@
+import { User } from "../../../../electron/src/sqllite/entities/user"
+
 export type logoutType = {
     message: string
+}
+
+export type setting = {
+    user: null | User
+    language: string
 }
