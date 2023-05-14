7eb732f5d3c3738993fe31de6be003f7d8c3e0c3
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2023-05-14 15:30:02 +0400

    create notfound page

---
+++ front/src/components/notFound/notFound.tsx
@@ -0,0 +1,19 @@
+import { NavLink, useLocation } from "react-router-dom"
+import { useTranslation } from "react-i18next"
+
+export const NotFound = () => {
+
+    const location = useLocation()
+    const { t } = useTranslation()
+
+    return <div className="flex flex-col justify-center items-center h-full">
+        <div className="max-w-5xl p-5 m-auto">
+            <p className="text-sm italic">{ t('Not Found URL: ') + location.pathname }</p>
+            <div className="flex">
+                <p className="pr-2">{ t('return to') }</p>
+                <NavLink to='/' className='underline italic'>{ t('Home') }</NavLink>
+                <p className="pl-2">{ t('page') }</p>
+            </div>
+        </div>
+    </div>
+}
